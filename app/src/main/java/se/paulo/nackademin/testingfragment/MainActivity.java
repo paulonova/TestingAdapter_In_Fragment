3d9aa510f8a93a13c18807fa0aebb7403c08823e
paulonova <paulonova144@gmail.com>
2016-06-28 00:26:15 +0200

    Initial commit

---
+++ app/src/main/java/se/paulo/nackademin/testingfragment/MainActivity.java
@@ -0,0 +1,13 @@
+package se.paulo.nackademin.testingfragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
